11821f91620a87bd0c8f821416b0fd9e4cefca23
liuyutongz <2664414121@qq.com>
2021-07-09 16:17:30 +0800

    first

---
+++ src/main/java/com/test/lyt/LytApplication.java
@@ -0,0 +1,14 @@
+package com.test.lyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 程序的主入口
+@SpringBootApplication
+public class LytApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LytApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/lyt/LytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.lyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//单元测试
+@SpringBootTest
+class LytApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
